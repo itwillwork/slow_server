88ce19992f53443fe86cd5f358adab8188817579
Edgar Nurullin <edgar.nurullin@onetwotrip.com>
2016-10-18 20:00:27 +0300

    fix

--- server.js
+++ server.js
@@ -14,18 +14,22 @@ app.get('/', function (req, res) {
     var i, j;
     var len = req.query.n;
     var arr = [];
-    for (i = 0; i < len; i++) {
-        arr.push(Math.random() * 100 ^ 0)
-    }
-    var n = arr.length;
-    for (i = 0; i < n-1; i++) {
-        for (j = 0; j < n-1-i; j++) {
-            if (arr[j+1] < arr[j]) {
-                var t = arr[j+1];
-                arr[j+1] = arr[j];
-                arr[j] = t;
+    try {
+        for (i = 0; i < len; i++) {
+            arr.push(Math.random() * 100 ^ 0)
+        }
+        var n = arr.length;
+        for (i = 0; i < n - 1; i++) {
+            for (j = 0; j < n - 1 - i; j++) {
+                if (arr[j + 1] < arr[j]) {
+                    var t = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = t;
+                }
             }
         }
+    } catch (e) {
+        arr = e;
     }
     res.status(200);
     res.send('ok. Random number ' + (Math.random() * 100 ^ 0) + ' \n' + JSON.stringify(arr));
