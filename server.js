76ba04f76912ad2479f21bf975a7a938c6bfe453
Edgar Nurullin <edgar.nurullin@onetwotrip.com>
2016-10-21 18:30:35 +0300

    attempt

--- server.js
+++ server.js
@@ -1,36 +1,27 @@
 var express = require('express');
 var app = express();
 
+var bcrypt = require('bcrypt');
+
 var dd_options = {
     'response_code':true,
     'tags': ['app:my_app']
 };
-
 var connect_datadog = require('connect-datadog')(dd_options);
-
 app.use(connect_datadog);
 
 app.get('/', function (req, res) {
-    var i, j;
-    var len = req.query.n;
-    var arr = [];
+    var iterate = req.query.n || 1;
+    var salt = bcrypt.genSaltSync(10);
+    var hash = "my password lol";
+    var i;
 
-    for (i = 0; i < len; i++) {
-        arr.push(Math.random() * 100 ^ 0)
-    }
-    var n = arr.length;
-    for (i = 0; i < n - 1; i++) {
-        for (j = 0; j < n - 1 - i; j++) {
-            if (arr[j + 1] < arr[j]) {
-                var t = arr[j + 1];
-                arr[j + 1] = arr[j];
-                arr[j] = t;
-            }
-        }
+    for (i = 0; i < iterate; i++) {
+        hash = bcrypt.hashSync(hash, salt);
     }
 
     res.status(200);
-    res.send('ok. Random number \n' + arr[0]);
+    res.send('ok. Random number \n' + hash);
 });
 
 app.listen(80, function () {
